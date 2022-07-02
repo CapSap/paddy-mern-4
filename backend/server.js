f2d0ab38c666135ba29bfd3b71dd4a6d689a4fd9
CapSap <cshotam@gmail.com>
2022-07-02 16:13:02 +1000

    add express cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const { errorLogger } = require("./middleware/errorLogger");
 const { errroHandler } = require("./middleware/errorMiddleware");
@@ -8,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 connectDB();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
