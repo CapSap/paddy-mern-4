78195f84c8f94c400f5cec28f104baf515608044
CapSap <cshotam@gmail.com>
2022-07-02 16:07:59 +1000

    get all orders from mongo (blocked by cors)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StoreChooser from "./components/StoreChooser";
+import Todos from "./components/Todos";
 
 function App() {
   const [store, setStore] = useState("");
 
+  const [allOrders, setAllOrders] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/orders", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => setAllOrders(data));
+  }, []);
+
   return (
     <div>
       <StoreChooser store={store} setStore={setStore} />
+      <Todos store={store} />
     </div>
   );
 }
