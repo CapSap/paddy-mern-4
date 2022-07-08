575413a6ae72f458d148b86ee5c6b7855d1472a6
CapSap <cshotam@gmail.com>
2022-07-08 15:27:16 +1000

    basic styling for card

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,12 +1,20 @@
 const Card = ({ order }) => {
-  console.log(order);
+  const items = order.orderedItems.map((x) => {
+    return (
+      <p key={x.sku}>
+        Sku: {x.sku} Qty: {x.qty} Descripton: {x.desc}
+      </p>
+    );
+  });
+
   return (
-    <div>
-      <div>
-        Order #: {order.orderNumber}
-        Name: {order.customerName}
-        Ph: {order.phone}
+    <div className="p-2 w-1/2 border">
+      <div className="bg-slate-300 ">
+        Order #: {order.orderNumber}| Name: {order.customerName} | Ph:{" "}
+        {order.phone}
       </div>
+      <div>Status</div>
+      <div>{items}</div>
     </div>
   );
 };
