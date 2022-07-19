3b748de37533df691e3216df09b025d79a74572f
CapSap <cshotam@gmail.com>
2022-07-19 15:15:26 +1000

    basic styling on entry comp

--- frontend/src/components/Entry.js
+++ frontend/src/components/Entry.js
@@ -55,55 +55,90 @@ function Entry() {
   const itemRequester = [];
   for (let i = 0; i < itemCounter; i++) {
     itemRequester.push(
-      <>
-        <label htmlFor="sku">Sku: </label>
-        <input
-          name="sku"
-          id={i}
-          type="text"
-          onChange={(e) => {
-            handleOrderedItemsChange(e);
-          }}
-        ></input>
-        <label htmlFor="qty">Quantity: </label>
-        <input
-          id={i}
-          name="qty"
-          type="Number"
-          onChange={(e) => {
-            handleOrderedItemsChange(e);
-          }}
-        ></input>
-        <label htmlFor="description">Description: </label>
-        <textarea
-          id={i}
-          name="description"
-          key={"description" + i}
-          onChange={(e) => {
-            handleOrderedItemsChange(e);
-          }}
-        />
-        <label htmlFor="sendingStore">Sending Store: </label>
-        <select
-          id={i}
-          name="sendingStore"
-          key={"sendingStore" + i}
-          value={orderInfo.orderedItems[i].sendingStore}
-          onChange={(e) => {
-            handleOrderedItemsChange(e);
-          }}
-        >
-          <option value="Canberra">Canberra - 213</option>
-          <option value="Fortitude Valley">Fortitude Valley - 416</option>
-          <option value="Hobart">Hobart - 710</option>
-          <option value="Melbourne">Melbourne - 314</option>
-          <option value="Seven Hills">Seven Hills - 208</option>
-          <option value="Perth">Perth - 615</option>
-          <option value="Ringwood">Ringwood - 319</option>
-          <option value="Sydney">Sydney - 210</option>
-        </select>
+      <div className="p-4 bg-slate-100">
+        <p>Request number {i + 1}</p>
+        <div>
+          <label
+            className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+            htmlFor="sku"
+          >
+            Sku:{" "}
+          </label>
+          <input
+            className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+            name="sku"
+            id={i}
+            type="text"
+            onChange={(e) => {
+              handleOrderedItemsChange(e);
+            }}
+          ></input>
+        </div>
+        <div>
+          <label
+            className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+            htmlFor="qty"
+          >
+            Quantity:{" "}
+          </label>
+          <input
+            className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+            id={i}
+            name="qty"
+            type="Number"
+            onChange={(e) => {
+              handleOrderedItemsChange(e);
+            }}
+          ></input>
+        </div>
+
+        <div>
+          <label
+            className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+            htmlFor="description"
+          >
+            Item description:{" "}
+          </label>
+          <textarea
+            className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+            id={i}
+            name="description"
+            key={"description" + i}
+            onChange={(e) => {
+              handleOrderedItemsChange(e);
+            }}
+          />
+        </div>
+        <div>
+          <label
+            className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+            htmlFor="sendingStore"
+          >
+            Sending Store:{" "}
+          </label>
+          <select
+            className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+            id={i}
+            name="sendingStore"
+            key={"sendingStore" + i}
+            value={orderInfo.orderedItems[i].sendingStore}
+            onChange={(e) => {
+              handleOrderedItemsChange(e);
+            }}
+          >
+            <option value="Canberra">Canberra - 213</option>
+            <option value="Fortitude Valley">Fortitude Valley - 416</option>
+            <option value="Hobart">Hobart - 710</option>
+            <option value="Melbourne">Melbourne - 314</option>
+            <option value="Seven Hills">Seven Hills - 208</option>
+            <option value="Perth">Perth - 615</option>
+            <option value="Ringwood">Ringwood - 319</option>
+            <option value="Sydney">Sydney - 210</option>
+          </select>
+        </div>
+
         <br />
-      </>
+      </div>
     );
   }
 
@@ -125,71 +160,148 @@ function Entry() {
   }
 
   return (
-    <div className=" bg-slate-300">
-      <div>
-        <h3>Paddy CNC app</h3>
-        <p>Requests to stores can be inputed here </p>
+    <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
+      <div className="mb-10 md:mb-16">
+        <h2 className="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">
+          Make requests below
+        </h2>
+        <p className="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">
+          Below is a form that will send a request to stores for CNC orders.
+          Feedback is welcome
+        </p>
       </div>
-      <form method="POST" action="/" onSubmit={(e) => onFormSubmit(e)}>
-        <label htmlFor="orderNumber">Order Number: </label>
-        <input
-          type="text"
-          name="orderNumber"
-          id="orderNumber"
-          value={orderInfo.orderNumber}
-          onChange={(e) => handleChange(e)}
-          autoComplete="given-name"
-        />
-        <label htmlFor="customerName">Customer Name: </label>
-        <input
-          type="text"
-          name="customerName"
-          id="customerName"
-          value={orderInfo.customerName}
-          onChange={(e) => handleChange(e)}
-          autoComplete="family-name"
-        />
-        <label htmlFor="pickupLocation">Pickup Location: </label>
-        <select
-          id="pickupLocation"
-          value={orderInfo.pickupLocation}
-          onChange={(e) => {
-            handleChange(e);
-          }}
-        >
-          <option value="Canberra">Canberra - 213</option>
-          <option value="Fortitude Valley">Fortitude Valley - 416</option>
-          <option value="Hobart">Hobart - 710</option>
-          <option value="Melbourne">Melbourne - 314</option>
-          <option value="Parramatta">Parramatta - 208</option>
-          <option value="Perth">Perth - 615</option>
-          <option value="Ringwood">Ringwood - 319</option>
-          <option value="Sydney">Sydney - 210</option>
-        </select>{" "}
-        <label htmlFor="fourHour">4 Hour? </label>
-        <input
-          id="fourHour"
-          name="fourHour"
-          type="checkbox"
-          value={orderInfo.fourHour}
-          onChange={(e) => handleChange(e)}
-        ></input>
+      <form
+        method="POST"
+        action="/"
+        onSubmit={(e) => onFormSubmit(e)}
+        className="max-w-screen-md grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto"
+      >
         <div>
-          <label htmlFor="notes">Notes: </label>
+          <label
+            className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+            htmlhtmlFor="orderNumber"
+          >
+            Order Number:{" "}
+          </label>
+          <input
+            required="true"
+            type="text"
+            name="orderNumber"
+            id="orderNumber"
+            value={orderInfo.orderNumber}
+            onChange={(e) => handleChange(e)}
+            className=" w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+          />
+        </div>
+        <div>
+          <label
+            htmlhtmlFor="customerName"
+            className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+          >
+            Customer Name:{" "}
+          </label>
+          <input
+            required="true"
+            type="text"
+            name="customerName"
+            id="customerName"
+            value={orderInfo.customerName}
+            onChange={(e) => handleChange(e)}
+            className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+          />
+        </div>
+        <div>
+          <label
+            className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+            htmlFor="pickupLocation"
+          >
+            Pickup Location:{" "}
+          </label>
+          <select
+            id="pickupLocation"
+            value={orderInfo.pickupLocation}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+          >
+            <option value="Canberra">Canberra - 213</option>
+            <option value="Fortitude Valley">Fortitude Valley - 416</option>
+            <option value="Hobart">Hobart - 710</option>
+            <option value="Melbourne">Melbourne - 314</option>
+            <option value="Parramatta">Parramatta - 208</option>
+            <option value="Perth">Perth - 615</option>
+            <option value="Ringwood">Ringwood - 319</option>
+            <option value="Sydney">Sydney - 210</option>
+          </select>{" "}
+        </div>
+        <div>
+          <label
+            htmlFor="fourHour"
+            className="text-gray-800 text-sm sm:text-base "
+          >
+            4 Hour:
+          </label>
+          <div className="mt-4 space-y-4">
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="fourHour"
+                  name="fourHour"
+                  type="checkbox"
+                  value={orderInfo.fourHour}
+                  onChange={(e) => handleChange(e)}
+                  className="ml-2 focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                ></input>
+              </div>
+              <div className="ml-3 text-sm">
+                <label htmlFor="fourHour" className="font-medium text-gray-700">
+                  Check this if the customer is expecting order to be ready
+                  within 4 hours.
+                </label>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="md:col-span-2">
+          <label
+            htmlhtmlFor="notes"
+            className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+          >
+            Notes:{" "}
+          </label>
           <textarea
             type="text"
             name="notes"
             id="notes"
             value={orderInfo.notes}
             onChange={(e) => handleChange(e)}
+            className="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
           ></textarea>
         </div>
-        <div>
-          Make multiple requests:{" "}
-          <button onClick={(e) => handleClick(e)}> Get more requests</button>
+        <div className="md:col-span-2 bg-slate-300 flex flex-col justify-center items-center">
+          {" "}
+          <p>Make multiple requests: </p>
+          <button
+            type="button"
+            onClick={(e) => handleClick(e)}
+            className="inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+          >
+            {" "}
+            Get more requests
+          </button>
         </div>
+        <p className="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">
+          Make a request for each sku on the order
+        </p>
         {itemRequester}
-        <button type="submit">Send order/request to store</button>
+        <button
+          className="md:col-span-2 inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+          type="submit"
+        >
+          Send order/request to store
+        </button>
       </form>
     </div>
   );
