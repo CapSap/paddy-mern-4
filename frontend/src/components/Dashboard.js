5da698fb6160d0cb84706690b048621f0442888e
CapSap <cshotam@gmail.com>
2022-07-31 09:32:02 +1000

    added more headings

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,31 +1,78 @@
-const Dashboard = ({ orders, filter }) => {
-  console.log(filter);
+const Dashboard = ({ orders, filterBy }) => {
+  console.log(orders);
   const data = orders
-    .filter((order) => order.status === filter)
+    .filter((order) => order.status === filterBy)
     .map((order) => (
-      <tr key={order._id} className="border">
-        <td>{new Date(order.createdAt).toLocaleDateString()}</td>
-        <td>{order.orderNumber}</td>
-        <td>{order.customerName}</td>
-        <td>{order.status}</td>
-        {console.log(order)}
-      </tr>
+      <>
+        {/* <tr key={order._id} className="border">
+          {console.log(order.orderedItems)}
+          <td>{new Date(order.createdAt).toLocaleDateString()}</td>
+          <td>{order.orderNumber}</td>
+          <td>{order.customerName}</td>
+          <td>{order.status}</td>
+          <td>{order.forHour}</td>
+          <td>{order.pickupLocation}</td>
+        </tr>
+
+        {order.orderedItems.map((item) => {
+          return (
+            <>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>{item.sendingStore}</td>
+                <td>{item.items}</td>
+              </tr>
+            </>
+          );
+        })} */}
+
+        {order.orderedItems.map((item) => {
+          return (
+            <tr key={order._id} className="border">
+              {console.log(order.orderedItems)}
+              <td>{new Date(order.createdAt).toLocaleDateString()}</td>
+              <td>{order.orderNumber}</td>
+              <td>{order.customerName}</td>
+              <td>{order.status}</td>
+              <td>{order.forHour}</td>
+              <td>{order.pickupLocation}</td>
+              <td>{item.sendingStore}</td>
+              <td>{item.items}</td>
+            </tr>
+          );
+        })}
+      </>
     ));
 
   return (
     <div>
-      Dashboard displaying orders with status: {filter}
-      <table className="table-auto border ">
-        <thead className="bg-gray-100">
-          <tr>
-            <th>Date</th>
-            <th>Order</th>
-            <th>Customer Name</th>
-            <th>Order Status</th>
-          </tr>
-        </thead>
-        <tbody>{data}</tbody>
-      </table>
+      {data.length ? (
+        <table className="table-auto border w-full text-center">
+          <thead className="bg-gray-100">
+            <tr>
+              <th>Date</th>
+              <th>Order</th>
+              <th>Customer Name</th>
+              <th>Order Status</th>
+              <th>Four Hour?</th>
+              <th>Collection Store</th>
+              <th>Sending Store</th>
+              <th>Items</th>
+              <th>IBT</th>
+              <th>Tracking</th>
+            </tr>
+          </thead>
+
+          <tbody>{data}</tbody>
+        </table>
+      ) : (
+        <p>No orders found with status {filterBy}</p>
+      )}
     </div>
   );
 };
