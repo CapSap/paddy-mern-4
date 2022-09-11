1920d5de338d8065bc885cce0b5cfb0d5259efc9
CapSap <cshotam@gmail.com>
2022-09-11 10:06:26 +1000

    remove some left over consolelogs

--- frontend/src/components/AwaitingEcommResponse.js
+++ frontend/src/components/AwaitingEcommResponse.js
@@ -3,12 +3,9 @@ import Card from "./Card";
 const AwaitingEcommResponse = ({ store, orders }) => {
   const awaitingOrders = orders.filter((order) => {
     return order.orderedItems.some((item) => {
-      console.log(item.message !== null);
       return item.sendingStore === store && item.message;
     });
   });
-
-  console.log(awaitingOrders);
 
   return (
     <div>
