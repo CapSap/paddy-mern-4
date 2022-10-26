7a6932acb44a8f1ddc9bb34b63e515824f8a6f3e
CapSap <cshotam@gmail.com>
2022-10-26 11:27:49 +1100

    add onchange functions and

--- frontend/src/components/EcommAdminPage/IssueCard.js
+++ frontend/src/components/EcommAdminPage/IssueCard.js
@@ -10,8 +10,14 @@ const IssueCard = ({ order }) => {
 
   const [newRequest, setNewRequest] = useState({
     sendingStore: "",
-    items: "",
+    items: order.orderedItems[0].items,
   });
+
+  const orderRequestIssues = order.orderedItems.filter((request) =>
+    request.requestStatus.includes("issue")
+  );
+
+  console.log(order);
 
   /*
 
@@ -25,10 +31,19 @@ const IssueCard = ({ order }) => {
   newRequest: { sendingStore: "", items: "" },
 
   */
-  function showHide() {
-    console.log("click");
-  }
 
+  const onChange = (e) =>
+    setNewRequest({
+      ...newRequest,
+      [e.target.name]: e.target.value,
+    });
+
+  const onOldChange = (e) => {
+    setOldRequest({
+      ...oldRequest,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <div className="border border-red-300 m-4">
       <div className="flex">
@@ -43,111 +58,105 @@ const IssueCard = ({ order }) => {
       </div>
       <div className="border-blue-300 border">
         <strong>Requests with issue:</strong>
-        {order.orderedItems
-          .filter((request) => request.requestStatus.includes("issue"))
-          .map((request) => {
-            return (
-              <div key={request.id} className="border-green-300 border m-2">
-                <p>Sending Store: {request.sendingStore}</p>
-                <p>
-                  Items requested: <strong>{request.items}</strong>
-                </p>
-                <p>
-                  Message: <strong>{request.message}</strong>
-                </p>
-                <p>Status: {request.requestStatus}</p>
+        {orderRequestIssues.map((request) => {
+          return (
+            <div key={request.id} className="border-green-300 border m-2">
+              <p>Sending Store: {request.sendingStore}</p>
+              <p>
+                Items requested: <strong>{request.items}</strong>
+              </p>
+              <p>
+                Message: <strong>{request.message}</strong>
+              </p>
+              <p>Status: {request.requestStatus}</p>
 
-                <form>
-                  <div className="flex">
-                    <div className="flex flex-col">
-                      <label htmlFor="storeChooser">Reasign request to: </label>
-                      <select
-                        id="storeChooser"
-                        name="storeChooser"
-                        defaultValue={""}
-                        onChange={(e) =>
-                          setNewRequest({
-                            ...newRequest,
-                            sendingStore: e.target.value,
-                          })
-                        }
-                      >
-                        <option disabled={true} value={""}>
-                          Select an option{" "}
-                        </option>
-                        <option value="Canberra">Canberra - 213</option>
-                        <option value="Fortitude Valley">
-                          Fortitude Valley - 416
-                        </option>
-                        <option value="Hobart">Hobart - 710</option>
-                        <option value="Melbourne">Melbourne - 314</option>
-                        <option value="Perth">Perth - 615</option>
-                        <option value="Ringwood">Ringwood - 319</option>
-                        <option value="Seven Hills">Seven Hills - 208</option>
-                        <option value="Sydney">Sydney - 210</option>
-                      </select>
-                      <span>
-                        Pleae select a store where a brand new request will be
-                        sent to {newRequest.sendingStore}
-                      </span>
+              <form>
+                <div className="flex">
+                  <div className="flex flex-col">
+                    <label htmlFor="sendingStore">Reasign request to: </label>
+                    <select
+                      id="sendingStore"
+                      name="sendingStore"
+                      defaultValue={""}
+                      onChange={onChange}
+                    >
+                      <option disabled={true} value={""}>
+                        Select an option{" "}
+                      </option>
+                      <option value="Canberra">Canberra - 213</option>
+                      <option value="Fortitude Valley">
+                        Fortitude Valley - 416
+                      </option>
+                      <option value="Hobart">Hobart - 710</option>
+                      <option value="Melbourne">Melbourne - 314</option>
+                      <option value="Perth">Perth - 615</option>
+                      <option value="Ringwood">Ringwood - 319</option>
+                      <option value="Seven Hills">Seven Hills - 208</option>
+                      <option value="Sydney">Sydney - 210</option>
+                    </select>
+                    <span>
+                      Pleae select a store where a brand new request will be
+                      sent to {newRequest.sendingStore}
+                    </span>
 
-                      <label
-                        className="inline-block text-gray-800 text-sm sm:text-base mb-2"
-                        htmlFor="items"
-                      >
-                        Item(s):{" "}
-                      </label>
-                      <textarea
-                        required={true}
-                        className=" resize bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
-                        name="items"
-                        key={"items"}
-                        defaultValue={request.items}
-                        onChange={(e) =>
-                          setNewRequest({
-                            ...newRequest,
-                            items: e.target.value,
-                          })
-                        }
-                      />
-                      <span>
-                        {newRequest.sendingStore
-                          ? `These items will appear in the request to
+                    <label
+                      className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+                      htmlFor="items"
+                    >
+                      Item(s):{" "}
+                    </label>
+                    <textarea
+                      required={true}
+                      className=" resize bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+                      name="items"
+                      key={"items"}
+                      defaultValue={request.items}
+                      onChange={(e) =>
+                        setNewRequest({
+                          ...newRequest,
+                          items: e.target.value,
+                        })
+                      }
+                    />
+                    <span>
+                      {newRequest.sendingStore
+                        ? `These items will appear in the request to
                       ${newRequest.sendingStore}`
-                          : "Choose a store first from the drop down"}
-                      </span>
-                    </div>
-
-                    <div className="flex flex-col">
-                      <p>Cancel or modify the original item request</p>
-
-                      <label
-                        className="inline-block text-gray-800 text-sm sm:text-base mb-2"
-                        htmlFor="items"
-                      >
-                        {request.sendingStore} Item(s):{" "}
-                      </label>
-                      <textarea
-                        required={true}
-                        className=" resize bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
-                        name="items"
-                        key={"items"}
-                        defaultValue={request.items}
-                      />
-                    </div>
-                    <div>
-                      <button
-                        className="md:col-span-2 inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
-                        type="submit"
-                      >
-                        Update the request for all stores
-                      </button>
-                    </div>
+                        : "Choose a store first from the drop down"}
+                    </span>
                   </div>
-                </form>
-              </div>
-            );
-          })}
+
+                  <div className="flex flex-col">
+                    <p>Modify the original item request</p>
+
+                    <label
+                      className="inline-block text-gray-800 text-sm sm:text-base mb-2"
+                      htmlFor="items"
+                    >
+                      {request.sendingStore} Item(s):{" "}
+                    </label>
+                    <textarea
+                      required={true}
+                      className=" resize bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+                      name="items"
+                      key={"items"}
+                      defaultValue="Dont post anything (cancel entire request)"
+                      onChange={onOldChange}
+                    />
+                  </div>
+                  <div>
+                    <button
+                      className="md:col-span-2 inline-block bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus-visible:ring ring-indigo-300 text-white text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3"
+                      type="submit"
+                    >
+                      Update the request for all stores
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
