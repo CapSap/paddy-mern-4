8c1a52dee0f66c40d8eaad6a79f2667613cf5359
CapSap <cshotam@gmail.com>
2022-11-14 11:31:24 +1100

    move filter to home comp

--- frontend/src/components/HomePage/Todos.js
+++ frontend/src/components/HomePage/Todos.js
@@ -3,25 +3,12 @@ import TodoCard from "./TodoCard";
 const Todos = (props) => {
   // props contains store and AllOrders from app.js
 
-  const orderTodos = props.orders.filter((order) => {
-    return order.orderedItems.some(
-      (item) =>
-        item.sendingStore === props.store && item.requestStatus == "created"
-    );
-  });
-
-  const fourHourTodos = props.orders.filter(
-    (order) => order.pickupLocation === props.store
-  );
-
-  console.log(fourHourTodos);
-
   return (
     <div>
-      {orderTodos.length > 0 ? (
+      {props.orders.length > 0 ? (
         <>
           <h3 className="font-bold">Todos for {props.store}</h3>
-          {orderTodos.map((order) => (
+          {props.orders.map((order) => (
             <TodoCard
               key={order._id}
               order={order}
