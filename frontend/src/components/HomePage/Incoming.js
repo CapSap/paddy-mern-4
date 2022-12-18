d204d13b7af36b0bdd4656a5b0eb64d161c389bf
CapSap <cshotam@gmail.com>
2022-12-18 17:43:54 +1100

    remove comment

--- frontend/src/components/HomePage/Incoming.js
+++ frontend/src/components/HomePage/Incoming.js
@@ -6,14 +6,6 @@ const Incoming = (props) => {
   );
 
   return (
-    // why does below need to be a table?
-    // maybe try cards?
-    // what is the goal?
-    // easy for people to read and use.
-    // responsive ie fit those stupid narrow screens?
-    // i think cards would be better.
-    // so how to display the data?
-
     <div className="flex-1">
       {filteredOrders.length ? (
         <>
